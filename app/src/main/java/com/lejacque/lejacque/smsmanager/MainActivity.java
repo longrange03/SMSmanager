c4cdcc6a0af5db0254d7ad4a81a52047c4d577e3
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-02 17:29:41 -0500

    Initial commit

---
+++ app/src/main/java/com/lejacque/lejacque/smsmanager/MainActivity.java
@@ -0,0 +1,70 @@
+package com.lejacque.lejacque.smsmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.lejacque.lejacque.smsmanager.R;
+
+public class MainActivity extends Activity {
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.button1);
+        txtphoneNo = (EditText) findViewById(R.id.edittext1);
+        txtMessage = (EditText) findViewById(R.id.edittext2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                String phoneNo = txtphoneNo.getText().toString();
+                String message = txtMessage.getText().toString();
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, message, null,
+                            null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG)
+                            .show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
